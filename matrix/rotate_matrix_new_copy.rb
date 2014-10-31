17f037268e235ec268bb09482c83a67e9ac71fb3
Jonathan Jin <jjin082693@gmail.com>
2014-10-31 16:24:30 -0500

    cleanup

--- matrix/rotate_matrix_new_copy.rb
+++ matrix/rotate_matrix_new_copy.rb
@@ -1,5 +1,6 @@
 # QUESTION: Write a function that, given an m x n array, the value of m, and the
-# value of n, returns a copy of said array rotated by 90 degrees clockwise.
+#           value of n, returns a copy of said array rotated by 90 degrees
+#           clockwise.
 #
 # AUTHOR: Jonathan Jin
 
